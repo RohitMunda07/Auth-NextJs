97016f070f5ce4411c002921b55d51c1118445a5
RohitMunda07 <rohitkumar14818@gmail.com>
2025-12-25 22:14:50 +0530

    feat: implement user model and signup API with password hashing

---
+++ src/models/user.model.js
@@ -0,0 +1,45 @@
+import { verify } from "crypto";
+import mongoose from "mongoose"
+const userSchema = mongoose.Schema({
+    userName: {
+        type: String,
+        required: [true, "Username is required"],
+        unique: true
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"]
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    },
+
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    forgotPasswordToken: {
+        type: String
+    },
+    forgotPasswordTokenExpiry: {
+        type: Date
+    },
+    verifyToken: {
+        type: String
+    },
+    verifyTokenExpiry: {
+        type: Date
+    },
+
+}, {
+    timestamps: true
+})
+
+const User = mongoose.models.users || mongoose.model("users", userSchema);
+export default User;
